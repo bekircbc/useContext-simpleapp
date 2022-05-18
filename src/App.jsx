221d7786e134072dacd1f6ed9fc0a70fe6104ac7
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-18 19:48:01 +0200

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import { Customers } from "./components/Customers";
+import { Employees } from "./components/Employees";
 
 function App() {
-  return <div className="App">Hello!</div>;
+  return (
+    <div className="App">
+      <h1>Test Question</h1>
+      <Customers />
+      <Employees />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/AppContext.jsx
@@ -0,0 +1,17 @@
+import { createContext } from "react";
+
+export const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const companyName = "Hamburg Robotics GmbH";
+
+  return (
+    <AppContext.Provider
+      value={{
+        companyName,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
