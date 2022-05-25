5fed217e4d79378efa6ee225a9c71f9303f321d4
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-26 00:51:09 +0200

    icon added to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,10 @@ import { Employees } from "./components/Employees";
 function App() {
   return (
     <div className="App">
-      <h1>Test Question</h1>
+      <div className="title">
+        <img src="images/icon.png" alt="logo" />
+        <h1>Simple Usecontext App</h1>
+      </div>
       <Customers />
       <Employees />
     </div>
